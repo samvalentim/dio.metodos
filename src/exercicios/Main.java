ab9121b9e0887576ce4c1f0da736c6d314b09aff
samvalentim <samanthamonteiro21@gmail.com>
2022-07-16 11:43:13 -0300

    Initial commit

---
+++ src/exercicios/Main.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+public class Main {
+    public static void manin (String[] args) {
+
+        // Calculadora
+        System.out.println("Exercício Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("Exercício Mensagem");
+        Mensagem.obterMensagem(  9);
+        Mensagem.obterMensagem (  14);
+        Mensagem.obterMensagem(  1);
+
+        //Emprestimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular( 1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular( 1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular( 1000,  5);
+
+
+    }
+}
